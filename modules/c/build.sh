c0b0fdafa12d04ef584c8d54f7995309239c0425
Marc Zweiler <marc.zweiler@outlook.at>
2025-08-23 12:20:51 +0200

    Added explicit c standard and made FIP posix compliant

--- modules/c/build.sh
+++ modules/c/build.sh
@@ -4,8 +4,8 @@ ROOT="$(pwd)";
 
 cd "$ROOT/modules/c" || exit 1
 
-clang "-I$ROOT" -g -o example_master example_master.c "$ROOT/toml/tomlc17.c" || exit 1
-clang "-I$ROOT" -g -o fip-c fip.c "$ROOT/toml/tomlc17.c" || exit 1
+clang "-I$ROOT" -g -std=c17 -o example_master example_master.c "$ROOT/toml/tomlc17.c" || exit 1
+clang "-I$ROOT" -g -std=c17 -o fip-c fip.c "$ROOT/toml/tomlc17.c" || exit 1
 
 cd "$ROOT" || exit 1
 
