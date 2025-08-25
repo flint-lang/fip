843979377449c4045d990c2773a9bd53eebbbd3f
Marc Zweiler <marc.zweiler@outlook.at>
2025-08-26 00:26:37 +0200

    Added struct type parsing and resolving to the fip-c modulef

--- modules/c/src/test.c
+++ modules/c/src/test.c
@@ -1,15 +1,27 @@
+#include <stddef.h>
 #include <stdio.h>
 
-int foo() {
-    return 0;
+typedef struct {
+    float x;
+    float y;
+    float z;
+} Vector3;
+
+Vector3 add_vectors(const Vector3 v1, const Vector3 v2) {
+    Vector3 result = v1;
+    result.x += v2.x;
+    result.y += v2.y;
+    result.z += v2.z;
+    return result;
 }
 
-int bar(int x, int y) {
+int foo(const int x, const int y) {
+    int *z = NULL;
+    printf("z = %p\n", z);
+    printf("*z = %i\n", *z);
     return x + y;
 }
 
-int main() {
-    printf("foo() = %i\n", foo());
-    printf("bar(1, 2) = %i\n", bar(1, 2));
-    return 0;
+int bar() {
+    return 69;
 }
